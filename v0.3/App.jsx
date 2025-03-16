4dc08d02eb31225ecf8f467602eb649356a8a223
Zihao <z@fv.cx>
2025-03-16 14:53:47 +0800

    diff: update children, delete redundant old nodes, and reduce unnecessary calculations

--- v0.3/App.jsx
+++ v0.3/App.jsx
@@ -1,4 +1,62 @@
 import React from "./core/React.js"
-const App = React.createElement('div', { id: 'app' }, 'app')
+
+let count = 10
+let showBar = false
+
+function Counter() {
+  const update = React.update()
+  const handleAdd = () => {
+    console.log('counter added')
+    count++
+    update()
+  }
+  return (
+    <div>
+      count:{count}
+      <button onClick={handleAdd}>add</button>
+    </div>
+  )
+}
+
+function ShowBar() {
+  function Foo() {
+    return (
+      <div>
+        foo
+        <div>child1</div>
+        <div>child2</div>
+      </div>
+    )
+  }
+
+  const bar = (
+    <p>bar</p>
+  )
+  const update = React.update()
+  const handleShowBar = () => {
+    console.log('showBar clicked')
+    showBar = !showBar
+    update()
+  }
+
+  return (
+    <div>
+      {showBar ? bar : <Foo/>}
+      {showBar && bar}
+      <button onClick={handleShowBar}>show bar</button>
+    </div>
+  )
+}
+
+function App() {
+  console.log('App rendered')
+  return (
+    <div>
+      hi-mini-react
+      <Counter/>
+      <ShowBar/>
+    </div>
+  )
+}
 
 export default App
--- v0.3/core/React.js
+++ v0.3/core/React.js
@@ -1,63 +1,254 @@
 function createTextNode(text) {
-    return {
-        type: 'TEXT_ELEMENT',
-        props: {
-            nodeValue: text,
-            children: []
-        }
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: []
     }
+  }
 }
-
-// const el = {
-//     type: 'div',
-//     props: {
-//         id: 'app',
-//         children: [textEl],
-//     }
-// }
 
 function createElement(type, props, ...children) {
-    return {
-        type,
-        props: {
-            ...props,
-            children: children.map((child) => {
-                return typeof child === 'string' ? createTextNode(child) : child
-            }),
-        }
+  return {
+    type,
+    props: {
+      ...props,
+      children: children.map((child) => {
+        const isTextNode = typeof child === 'string' || typeof child === 'number'
+        return isTextNode ? createTextNode(child) : child
+      }),
     }
+  }
 }
 
-
-// const dom = document.createElement(App.type)
-// dom.id = App.props.id
-// document.querySelector('#root').append(dom)
-
-// const textNode = document.createTextNode('')
-// textNode.nodeValue = textEl.props.nodeValue
-// dom.append(textNode)
-
-
 function render(el, container) {
-    const dom = el.type === "TEXT_ELEMENT" ? document.createTextNode('') : document.createElement(el.type)
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [el]
+    }
+  }
+  nextWorkOfUnit = wipRoot
+}
 
-    // id class
-    Object.keys(el.props).forEach((key) => {
-        if (key !== 'children') {
-            dom[key] = el.props[key]
+// work in progress
+let wipRoot = null
+let currentRoot = null
+let nextWorkOfUnit = null
+let deletions = []
+let wipFiber = null
+
+function workLoop(deadline) {
+  let shouldYield = false
+  while (!shouldYield && nextWorkOfUnit) {
+    nextWorkOfUnit = performWorkOfUnit(nextWorkOfUnit)
+
+    if (wipRoot?.sibling?.type === nextWorkOfUnit?.type) {
+      nextWorkOfUnit = undefined
+    }
+
+    shouldYield = deadline.timeRemaining() < 1
+  }
+
+  if (!nextWorkOfUnit && wipRoot) {
+    commitRoot()
+  }
+
+  requestIdleCallback(workLoop)
+}
+
+function commitRoot() {
+  deletions.forEach(commitDeletion)
+  commitWork(wipRoot.child)
+  currentRoot = wipRoot
+  wipRoot = null
+  deletions = []
+}
+
+function commitDeletion(fiber) {
+  if (fiber.dom) {
+    let fiberParent = fiber.parent
+    while (!fiberParent.dom) {
+      fiberParent = fiberParent.parent
+    }
+    fiberParent.dom.removeChild(fiber.dom)
+  } else {
+    commitDeletion(fiber.child)
+  }
+}
+
+function commitWork(fiber) {
+  if (!fiber) return
+
+  let fiberParent = fiber.parent
+  while (!fiberParent.dom) {
+    fiberParent = fiberParent.parent
+  }
+
+  if (fiber.effectTag === 'update') {
+    updateProps(fiber.dom, fiber.props, fiber.alternate?.props)
+  } else if (fiber.effectTag === 'placement') {
+    if (fiber.dom) {
+      fiberParent.dom.appendChild(fiber.dom)
+    }
+  }
+
+  commitWork(fiber.child)
+  commitWork(fiber.sibling)
+}
+
+function createDom(type) {
+  return type === "TEXT_ELEMENT" ? document.createTextNode('') : document.createElement(type)
+}
+
+function addEvents(dom, name, fn) {
+  dom["data-events"].push({name, fn});
+}
+
+function updateProps(dom, nextProps, prevProps) {
+  Object.keys(prevProps).forEach(key => {
+    if (key !== 'children') {
+      if (!(key in nextProps)) {
+        dom.removeAttribute(key)
+      }
+    }
+  })
+
+  Object.keys(nextProps).forEach((key) => {
+    if (key !== 'children') {
+      if (nextProps[key] !== prevProps[key]) {
+        if (key.startsWith('on')) {
+          const eventType = key.slice(2).toLowerCase()
+          dom.removeEventListener(eventType, prevProps[key])
+
+          if (!dom["data-events"]) {
+            dom["data-events"] = [];
+          }
+
+          addEvents(dom, nextProps[key].name, nextProps[key]);
+          if (!nextProps[key].id) {
+            nextProps[key].id = eventId;
+            eventId++
+          }
+
+          dom.addEventListener(eventType, nextProps[key])
+        } else {
+          dom[key] = nextProps[key]
         }
-    })
+      }
+    }
+  })
+}
 
-    const children = el.props.children
-    children.forEach(child => {
-        render(child, dom)
-    })
-    container.append(dom)
+let eventId = 1
+let fiberId = 0
+
+function reconcileChildren(fiber, children) {
+  let oldFiber = fiber.alternate?.child
+  let prevChild = null
+  children.forEach((child, index) => {
+    const isSameType = oldFiber && oldFiber.type === child.type
+
+    let newFiber
+    if (isSameType) {
+      // update
+      newFiber = {
+        type: child.type,
+        props: child.props,
+        child: null,
+        parent: fiber,
+        sibling: null,
+        dom: oldFiber.dom,
+        effectTag: 'update',
+        alternate: oldFiber,
+        id: oldFiber.id
+      }
+    } else {
+      // create
+      if (child) {
+        newFiber = {
+          type: child.type,
+          props: child.props,
+          child: null,
+          parent: fiber,
+          sibling: null,
+          dom: null,
+          effectTag: 'placement',
+          id: fiberId++
+        }
+      }
+
+      if (oldFiber) {
+        deletions.push(oldFiber)
+      }
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+    }
+
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      prevChild.sibling = newFiber
+    }
+
+    if (newFiber) {
+      prevChild = newFiber
+    }
+  })
+
+  while (oldFiber) {
+    deletions.push(oldFiber)
+
+    oldFiber = oldFiber.sibling
+  }
+}
+
+function updateFunctionComponent(fiber) {
+  wipFiber = fiber
+
+  const children = [fiber.type(fiber.props)]
+  reconcileChildren(fiber, children)
+}
+
+function updateHostComponent(fiber) {
+  if (!fiber.dom) {
+    const dom = (fiber.dom = createDom(fiber.type))
+    updateProps(dom, fiber.props, {})
+  }
+  const children = fiber.props.children
+  reconcileChildren(fiber, children)
+}
+
+function performWorkOfUnit(fiber) {
+  const isFunctionComponent = typeof fiber.type === 'function'
+  isFunctionComponent ? updateFunctionComponent(fiber) : updateHostComponent(fiber)
+
+  if (fiber.child) return fiber.child
+
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) return nextFiber.sibling
+    nextFiber = nextFiber.parent
+  }
+}
+
+requestIdleCallback(workLoop)
+
+function update() {
+  let currentFiber = wipFiber
+  return () => {
+    wipRoot = currentFiber
+    wipRoot.alternate = currentFiber
+    nextWorkOfUnit = wipRoot
+  }
 }
 
 const React = {
-    render,
-    createElement,
+  update,
+  render,
+  createElement,
 }
 
 export default React
